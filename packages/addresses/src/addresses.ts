513b7aecaa349ea47b734e56c1566162aca15649
nenadmitt <nenadmitt@gmail.com>
2025-03-31 23:14:26 +0200

    fix: New mint controller addresses

--- packages/addresses/src/addresses.ts
+++ packages/addresses/src/addresses.ts
@@ -30,13 +30,13 @@ export interface EnsContracts {
 
 const l1Contracts: Record<number, L1ContractAddresses> = {
     [mainnetId]: {
-        mintController: "0x313442ba3A0b12193787BD162f99Ed3C415F2886",
+        mintController: "0xCf7625A2fb60B0822444E5964b4Ce80c148e7Fad",
         nameWrapperProxy: "0x25ADB7e69390FbfeEe26F3C8053955d4D4428Afd",
         hybridResolver: "0x0",
         oldHybridResolver: "0x0dcD506D1Be162E50A2b434028A9a148F2686444"
     },
     [sepoliaId]: {
-        mintController: "0x1Ded316C799e1445894722F4C0FF2E6175d6AA8e",
+        mintController: "0x313442ba3A0b12193787BD162f99Ed3C415F2886",
         nameWrapperProxy: "0x0Ff41b99D7185B01bA47Ca85e9049166Cb3CD6bd",
         hybridResolver: "0x0",
         oldHybridResolver: "0x2F5E9E8B4495e4CDC5bC6C5FCBa93B8AaBDeF595"
